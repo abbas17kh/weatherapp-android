e496e29f2778a5b7c5b118a4ab85e80327402542
abbas17kh <halilabbs97@gmail.com>
2025-09-01 17:13:50 +0200

    .
    Signed-off-by: abbas17kh <halilabbs97@gmail.com>

--- app/src/main/java/abbas17kh/weatherapp/MainActivity.kt
+++ app/src/main/java/abbas17kh/weatherapp/MainActivity.kt
@@ -18,30 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TestWeatherappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            TestWeatherappTheme(dynamicColor = false) {
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestWeatherappTheme {
-        Greeting("Android")
     }
 }
